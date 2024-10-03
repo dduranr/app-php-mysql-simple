05a23f222ad4f6a9e91f0d01cd6ad076e0b76b56
David Durán <official.dduran@gmail.com>
2024-10-03 17:00:36 -0600

    Inicialización

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Tareas</title>
+		<link rel="stylesheet" type="text/css" href="assets/css/estilos.css?1">
+	</head>
+	<body>
+
+		<div class="contenedor">
+			<div class="fila">
+				<div class="columna-6">
+					<h2>Lista de tareas</h2>
+				</div>
+				<div class="columna-6">
+					<button id="crear-tarea">Crear tarea</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="contenedor">
+			<div class="fila">
+				<div class="columna-12">
+					<table border="1">
+						<thead>
+							<tr>
+								<th>ID</th>
+								<th>Task name</th>
+								<th>Created at</th>
+								<th></th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+						</tbody>
+						<tfoot>
+							<tr>
+								<th>ID</th>
+								<th>Task name</th>
+								<th>Created at</th>
+								<th></th>
+								<th></th>
+							</tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
